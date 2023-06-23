1302383319f69816b4909d515e39a532f0adf403
Riches T <richestrinh@gmail.com>
2023-06-22 17:23:13 -0700

    setup of express app and state hooks

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function RegisterPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  function registerUser() {
+    // Send request to API
+    
+  }
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto">
-          <input type="text" placeholder="John Doe" />
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+        {/* On submission, call registerUser function */}
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          
+          {/* On change of the fields update the state variables using the useState hook */}
+          <input type="text" placeholder="John Doe"
+            value={name}
+            onChange={ev => setName(ev.target.value)} />
+          <input type="email" placeholder="your@email.com"
+            value={email}
+            onChange={ev => setEmail(ev.target.value)} />
+          <input type="password" placeholder="password"
+            value={password}
+            onChange={ev => setPassword(ev.target.value)} />
+
           <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
             Already a member? <Link className="underline text-black" to={'/login'}>Login</Link>
