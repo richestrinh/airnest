5909177a84142d6d598a6662042a4b325e92cba2
Riches T <107906449+richestrinh@users.noreply.github.com>
2023-06-26 20:10:06 -0700

    - updated navbar styling - implemented adding of new place

---
+++ api/models/Place.js
@@ -0,0 +1,22 @@
+// Import the Mongoose library.
+const mongoose = require('mongoose');
+
+// Create a schema.
+const placeSchema = new mongoose.Schema({
+    owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    title: String,
+    address: String,
+    photos: [String],
+    description: String,
+    perks: [String],
+    extraInfo: String,
+    checkIn: Number,
+    checkOut: Number,
+    maxGuests: Number,
+});
+
+// Create a model from this schema.
+const PlaceModel = mongoose.model('Place', placeSchema);
+
+// Exports the model to be used in other parts of the application.
+module.exports = PlaceModel;
