6b95af2671dbdcb0cac5c89e3bb70e7186170285
Riches T <107906449+richestrinh@users.noreply.github.com>
2023-06-28 13:18:49 -0700

    - completed adding photo by device. - implemented selection of perks.

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 const cookieParaser = require('cookie-parser');
 const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
+
 require('dotenv').config();
 const app = express();
 
@@ -18,8 +21,8 @@ const jwtSecret = "asdasdbfyhcmiqwuhe";
 app.use(express.json());
 app.use(cookieParaser());
 // Display photos in the browser.
-app.use('/uploads', express.static(__dirname+'/uploads'));
-app.use(cors({  
+app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
 }));
@@ -32,37 +35,37 @@ app.get('/test', (req, res) => {
 
 app.post('/register', async (req, res) => {
     // Grab all information from the request body.
-    const {name, email, password} = req.body;
-    
+    const { name, email, password } = req.body;
+
     try {
         const userDoc = await User.create({
-            name, 
-            email, 
+            name,
+            email,
             // Generate and store hash in DB.
-            password:bcrypt.hashSync(password, bcryptSalt),
+            password: bcrypt.hashSync(password, bcryptSalt),
         });
         // Respond with json with all the information we are sending.
         res.json(userDoc);
     } catch (err) {
         // 422 is the HTTP status code for unprocessable entity.
         res.status(422).json(err);
-    }    
+    }
 });
 
 app.post('/login', async (req, res) => {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
     // Find user by email.
-    const userDoc = await User.findOne({email:email});
+    const userDoc = await User.findOne({ email: email });
     if (userDoc) {
         // Check if password is correct.
         const passOk = bcrypt.compareSync(password, userDoc.password);
         if (passOk) {
             // Create a token with the user's email and id.
             jwt.sign({
-                email:userDoc.email, 
-                id:userDoc._id, 
+                email: userDoc.email,
+                id: userDoc._id,
             }, jwtSecret, {}, (err, token) => {
-                if(err) throw err;
+                if (err) throw err;
                 // Respond with token (a cookie) from callback function and returns userDoc
                 res.cookie('token', token, 'none').json(userDoc);
             });
@@ -70,7 +73,7 @@ app.post('/login', async (req, res) => {
         else {
             res.status(422).json('pass not ok');
         }
-    } 
+    }
     else {
         res.json('not found');
     }
@@ -78,18 +81,18 @@ app.post('/login', async (req, res) => {
 
 app.get('/profile', (req, res) => {
     // We need to get the token from the cookie.
-    const {token} = req.cookies;
-    
+    const { token } = req.cookies;
+
     // If the token is valid, we can get the user from the token with our salt key.
     if (token) {
         jwt.verify(token, jwtSecret, {}, async (err, userData) => {
             if (err) throw err;
-            
+
             // Grab name, email and ID from the token.
-            const {name, email, _id} = await User.findById(userData.id);
-            res.json({name, email, _id});
+            const { name, email, _id } = await User.findById(userData.id);
+            res.json({ name, email, _id });
         });
-    } 
+    }
     else {
         res.json(null);
     }
@@ -102,14 +105,43 @@ app.post('/logout', (req, res) => {
 
 // Endpoint for uploading an image by a link. 
 app.post('/upload-by-link', async (req, res) => {
-    const {link} = req.body;
+    const { link } = req.body;
     const newName = 'photo' + Date.now() + '.jpg';
     await imageDownloader.image({
-        url:link, 
+        url: link,
         // Add full path to directory.
-        dest: __dirname + '/uploads/' +newName,
+        dest: __dirname + '/uploads/' + newName,
     });
     res.json(newName);
+});
+
+// Endpoint for uploading an image from a device.
+// Uses Multer middleware.
+const photosMiddleware = multer({ dest: 'uploads/' });
+app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
+    // Keep track of the number of files uploaded.
+    const uploadedFiles = [];
+
+    // Loop through all the files in the request.
+    for(let i = 0; i < req.files.length; i++) {
+        const { path, originalname } = req.files[i];
+        // Parts is the array of originalname parts.
+        const parts = originalname.split('.');
+
+        // Grab the file extension.
+        const ext = parts[parts.length - 1];
+
+        // Append ext.
+        const newPath = path + '.' + ext;
+
+        // Rename file.
+        fs.renameSync(path, newPath);
+
+        // Push the new path to the array. (Remove the 'uploads/')
+        uploadedFiles.push(newPath. replace('uploads/', ''));
+    }
+
+    res.json(uploadedFiles);
 });
 
 app.listen(4000);
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -46,6 +46,21 @@ export default function PlacesPage() {
     // Reset this state / input.
     setPhotoLink('');
   }
+  function uploadPhoto(ev) {
+    const files = ev.target.files;
+    const data = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      data.append('photos', files[i]);
+    }
+    axios.post('/upload', data, {
+      headers: { 'Content-type': 'multipart/form-data' }
+    }).then(res => {
+      const { data: filenames} = res;
+      setAddedPhoto(prev => {
+        return [...prev, ...filenames];
+      });
+    })
+  }
   return (
     <div>
       {/* Show add new place button if we haven't clicked it yet */}
@@ -87,19 +102,19 @@ export default function PlacesPage() {
             </div>
 
             {/* Upload Button and Album of added Photos*/}
-            {/* TODO: Photos that are different sizes show up differently. */}
-            <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+            <div className="h-32 mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
               {addedPhoto.length > 0 && addedPhoto.map(link => (
-                <div>
-                  <img className="rounded-2xl" src={'http://localhost:4000/uploads/'+link} />
+                <div className="h-32 flex">
+                  <img className="rounded-2xl w-full object-cover" src={'http://localhost:4000/uploads/' + link} />
                 </div>
               ))}
-              <button className="flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
+              <label className="flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600 cursor-pointer">
+                <input type="file" multiple className="hidden" onChange={uploadPhoto} />
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                 </svg>
                 Upload
-              </button>
+              </label>
             </div>
 
             {preInput('Description', 'description for your place.')}
