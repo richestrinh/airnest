e24527ca8f20aa1f0528fdabaf9437d9b2da24c7
Riches T <107906449+richestrinh@users.noreply.github.com>
2023-06-28 19:10:33 -0700

    - fixed missing key error - refactored PlacesPage - created PhotosUploader component

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,15 +1,14 @@
-import axios from "axios";
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Perks from "../Perks";
+import PhotosUploader from "../PhotosUploader";
 
 export default function PlacesPage() {
   // Grab action from URL
   const { action } = useParams();
   const [title, setTitle] = useState('');
   const [address, setAddress] = useState('');
-  const [addedPhoto, setAddedPhoto] = useState([]);
-  const [photoLink, setPhotoLink] = useState('');
+  const [addedPhotos, setAddedPhotos] = useState([]);
   const [description, setDescription] = useState('');
   const [perks, setPerks] = useState([]);
   const [extraInfo, setExtraInfo] = useState('');
@@ -34,33 +33,7 @@ export default function PlacesPage() {
       </>
     );
   }
-  async function addPhotoByLink(ev) {
-    // Prevent reloading of form after clicking button that calls this function.
-    ev.preventDefault();
-    // We need endpoint that will take the link and upload it to the server(uploads folder).
-    // Send 'photoLink' as 'link' to the endpoint.
-    const { data: filename } = await axios.post('/upload-by-link', { link: photoLink });
-    setAddedPhoto(prev => {
-      return [...prev, filename];
-    });
-    // Reset this state / input.
-    setPhotoLink('');
-  }
-  function uploadPhoto(ev) {
-    const files = ev.target.files;
-    const data = new FormData();
-    for (let i = 0; i < files.length; i++) {
-      data.append('photos', files[i]);
-    }
-    axios.post('/upload', data, {
-      headers: { 'Content-type': 'multipart/form-data' }
-    }).then(res => {
-      const { data: filenames} = res;
-      setAddedPhoto(prev => {
-        return [...prev, ...filenames];
-      });
-    })
-  }
+
   return (
     <div>
       {/* Show add new place button if we haven't clicked it yet */}
@@ -91,31 +64,9 @@ export default function PlacesPage() {
               onChange={ev => setAddress(ev.target.value)}
               placeholder="address" />
 
-            {/* Add by Link */}
             {preInput('Photos', 'photos to showcase your place')}
-            <div className="flex gap-2">
-              <input type="text"
-                value={photoLink}
-                onChange={ev => setPhotoLink(ev.target.value)}
-                placeholder="Add photo using a link (.jpg format)" />
-              <button onClick={addPhotoByLink} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
-            </div>
+            <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
 
-            {/* Upload Button and Album of added Photos*/}
-            <div className="h-32 mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-              {addedPhoto.length > 0 && addedPhoto.map(link => (
-                <div className="h-32 flex">
-                  <img className="rounded-2xl w-full object-cover" src={'http://localhost:4000/uploads/' + link} />
-                </div>
-              ))}
-              <label className="flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600 cursor-pointer">
-                <input type="file" multiple className="hidden" onChange={uploadPhoto} />
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-                </svg>
-                Upload
-              </label>
-            </div>
 
             {preInput('Description', 'description for your place.')}
             <textarea value={description} onChange={ev => setDescription(ev.target.value)} />
