d00a798e6d6588fb7517ccba6272f6244dbe6999
Riches T <107906449+richestrinh@users.noreply.github.com>
2023-08-18 14:43:13 -0700

    add vercel web analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './pages/App'
 import './styles/index.css'
+import { inject } from '@vercel/analytics';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
